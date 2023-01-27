d5d994b4995dd98376961402ba5283b36222ff11
zackhabar72 <zackhabar72@mail.ru>
2023-01-28 02:49:06 +0500

    Created now

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -1,2 +1,86 @@
-package PACKAGE_NAME;public class FirstTests {
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static org.testng.Assert.fail;
+
+public class FirstTests {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    private JavascriptExecutor js;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        //ERROR: Caught exception [unknown command []]
+        //ERROR: Caught exception [unknown command []]
+        driver.get("https://app.qase.io/login?redirectPath=%2Fprojects");
+        driver.findElement(By.id("inputEmail")).clear();
+        driver.findElement(By.id("inputEmail")).sendKeys("torezmoriz@gmail.com");
+        driver.findElement(By.id("inputPassword")).clear();
+        driver.findElement(By.id("inputPassword")).sendKeys("a.Zahar760");
+        driver.findElement(By.id("inputEmail")).click();
+        driver.findElement(By.id("inputPassword")).click();
+        driver.findElement(By.id("btnLogin")).click();
+        driver.get("https://app.qase.io/projects");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
+
